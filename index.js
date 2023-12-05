dc4a009fa074b740acb94500b470b9f143e0f7c1
Tu Nombre <you@example.com>
2023-12-04 18:25:26 -0600

    fix

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 
 // Conexión a la base de datos MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("Connected to MongoDB"))
   .catch(error => console.error("MongoDB connection error:", error));
 
